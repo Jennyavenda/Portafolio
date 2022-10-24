0483266aebf2638183e324469f68faaf33b48495
Jennyavenda <78169455+Jennyavenda@users.noreply.github.com>
2022-10-24 11:36:12 -0500

    Delete Serie.h

--- Serie.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include "Episodio.h"
-
-class Serie: public Episodio{
-    std::vector <Video> series;
-    public:
-    Serie(std::string i, std::string n, float d, std::string g, int c): Episodio(i, n, d, g, c){};
-};
